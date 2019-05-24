e1bdf6615b391414aa06af5e45e0ac75f451d15a
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-24 16:57:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,9 @@
 def unsafe?(speed)
-if speed > 60 || 
+if speed > 60 || speed < 40
   "true"
 else
   "false"
 end
-
-def unsafe?(speed) 
-  if speed < 40 
-    "true"
-else
-   "false"
-end
-
 
 
 def not_safe?(speed)
